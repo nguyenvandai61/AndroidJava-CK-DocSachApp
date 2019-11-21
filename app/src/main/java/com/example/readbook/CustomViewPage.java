ea8a8345d38de909a8be206a782117b3ef91a0bf
Trinhnguyen1704 <51841079+Trinhnguyen1704@users.noreply.github.com>
2019-11-21 10:11:27 +0700

    first commit

---
+++ app/src/main/java/com/example/readbook/CustomViewPage.java
@@ -0,0 +1,4 @@
+package com.example.readbook;
+
+public class CustomViewPage {
+}
---
+++ app/src/main/java/com/example/readbook/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.readbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
