aa2acc2daea9acafe9f2f9c77eb1d24e1ca8f240
Trinhnguyen1704 <51841079+Trinhnguyen1704@users.noreply.github.com>
2019-12-08 23:42:11 +0700

    search menu

--- app/src/main/java/com/example/readbook/ui/catalog/CatalogFragment.java
+++ app/src/main/java/com/example/readbook/ui/catalog/CatalogFragment.java
@@ -1,9 +1,13 @@
 package com.example.readbook.ui.catalog;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,11 +16,19 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.readbook.Model.Book;
 import com.example.readbook.R;
+import com.example.readbook.adapter.BookListAdapter;
+import com.example.readbook.ui.main.PlaceholderFragment;
+
+import java.util.ArrayList;
 
 public class CatalogFragment extends Fragment {
 
     private CatalogViewModel catalogViewModel;
+
+
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -32,4 +44,5 @@ public class CatalogFragment extends Fragment {
         });
         return root;
     }
+
 }
